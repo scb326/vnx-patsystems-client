b019554dac9ab89329b0ea396262f6e691df82b1
anngdo@gmail.com <anngdo@gmail.com@d819b74f-4f9c-00c9-2484-97dd7400dc5c>
2011-07-14 07:25:28 +0000

    add page demo2 to display another component (only for test)

--- trunk/MarketTradingDemo/JavaSource/com/market/trading/bean/QuoteBean.java
+++ trunk/MarketTradingDemo/JavaSource/com/market/trading/bean/QuoteBean.java
@@ -1,23 +1,27 @@
 package com.market.trading.bean;
 
-import java.util.ArrayList;
 import java.util.List;
+
 import javax.annotation.PostConstruct;
 
 import org.richfaces.component.html.HtmlDataTable;
-import org.richfaces.taglib.DataTableTag;
 
 import com.market.trading.bean.delegate.QuoteBeanDelegate;
 import com.market.trading.model.Quote;
 import com.vnx.patsystems.util.Feeder;
-import com.vnx.patsystems.util.Feeder.SubscribedItem;
 
 public class QuoteBean implements QuoteBeanDelegate{
-	
+	public static final String TRADING = "TRADE";
+	public static final String ORDERING = "ORDER";
 	private static boolean isFeederReady = false;
 	private List<Quote> quotes = null;
 	private int index = 0;
 	private HtmlDataTable tblQuote;
+	
+	/**
+	 * Trading or ordering. (When press button trade, order)
+	 */
+	private String status = TRADING;
 
 	public HtmlDataTable getTblQuote() {
 		return tblQuote;
@@ -29,6 +33,9 @@ public class QuoteBean implements QuoteBeanDelegate{
 
 	@PostConstruct
 	public void initData(){
+		//init status = trading in the first time
+		status = TRADING;
+		
 		if(!isFeederReady) {
 			isFeederReady = true;
 			new Feeder(this);
@@ -61,6 +68,22 @@ public class QuoteBean implements QuoteBeanDelegate{
 			quotes.get(index).setBid(updatedQuote.getBid());
 			quotes.get(index).setLast(updatedQuote.getLast());
 			quotes.get(index).setTickVol(updatedQuote.getTickVol());
+		}
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public void changeStatus(){
+		if(TRADING.equals(status)){
+			status = ORDERING;
+		}else{
+			status = TRADING;
 		}
 	}
 }
